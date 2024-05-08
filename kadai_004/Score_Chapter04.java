a5020b31beb366d33317c2a6e087627ccefc8890
yamagen0829 <yamagen3745@gmail.com>
2024-05-08 22:01:20 +0900

    Add kadai_004

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,18 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		System.out.println("Aさんの点数は10点");
+		System.out.println("Bさんの点数は20点");
+		System.out.println("Cさんの点数は30点");
+		System.out.println("Dさんの点数は40点");
+		System.out.println("Eさんの点数は50点");
+		System.out.println("Fさんの点数は60点");
+		System.out.println("Gさんの点数は70点");
+		System.out.println("Hさんの点数は80点");
+		System.out.println("Iさんの点数は90点");
+		System.out.println("Jさんの点数は100点");
+		System.out.println("テストの平均点は" + ((10+20+30+40+50+60+70+80+90+100)/10) + "点");
+	}
+}
