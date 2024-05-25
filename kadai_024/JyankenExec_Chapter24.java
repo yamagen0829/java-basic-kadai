aa6749f8eae092046c6626ee59cf6d8b73587725
yamagen0829 <yamagen3745@gmail.com>
2024-05-25 22:50:35 +0900

    Add kadai_024

---
+++ kadai_024/JyankenExec_Chapter24.java
@@ -0,0 +1,11 @@
+package kadai_024;
+
+public class JyankenExec_Chapter24 {
+
+	public static void main(String[] args) {
+		  Jyanken_Chapter24 Jyanken = new Jyanken_Chapter24();
+		  
+		  Jyanken.playGame();
+	}
+
+}
---
+++ kadai_024/Jyanken_Chapter24.java
@@ -0,0 +1,69 @@
+package kadai_024;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Jyanken_Chapter24 {
+	public String getMyChoice () {
+	//Scannerクラスのオブジェクトを生成する
+       Scanner scanner = new Scanner(System.in);
+       
+     while (true) {
+	   System.out.println("自分のじゃんけんの手を入力しましょう");
+	   System.out.println("グーはrockのrを入力しましょう");
+	   System.out.println("チョキはscissorsのsを入力しましょう");
+	   System.out.println("パーはpaperのpを入力しましょう");
+
+	   //入力した内容を取得
+	   String input = scanner.next();
+	 
+	 //正しいじゃんけんの手であるか
+	  if((input.equals("r")) || (input.equals("s")) || (input.equals("p"))) {
+		//Scannerクラスのオブジェクトをクローズする
+		   scanner.close();
+		   return input;
+	  } else {
+		System.out.println("r（グー）かs（チョキ）かp（パー）を入力してください。");
+	  }
+	 }	
+	}
+	
+	public String getRandom () {
+	   //配列にじゃんけんの手をセットする
+	   String[] opponent = {"r", "s", "p"};
+	   
+	   //対戦相手のじゃんけんの手を選ぶ
+	   int opponentJyanken = (int) Math.floor(Math.random()* opponent.length);
+		   return opponent[opponentJyanken];
+	 }
+	   
+	public void playGame () {
+	  HashMap<String,String> choice = new HashMap<String,String>();
+	  
+	   choice.put("r", "グー");
+	   choice.put("s", "チョキ");
+	   choice.put("p", "パー");
+	   
+	   String me = this.getMyChoice();
+	   String you = this.getRandom();
+	   
+	   System.out.println("自分の手は" + choice.get(me) + ",対戦相手は" + choice.get(you));
+	   
+	   //あいこ
+	   if(me.equals(you)) {
+		   System.err.println("あいこです");
+	   }
+	   
+	   //自分が勝った場合
+	   if ((me.equals("r") && you.equals("s")) || (me.equals("s") && you.equals("p")) || (me.equals("p") && you.equals("r"))) {
+		   System.out.println("自分の勝ちです");
+	   }
+	   
+	   //自分が負けた場合
+	   if ((me.equals("r") && you.equals("p")) || (me.equals("s") && you.equals("r")) || (me.equals("p") && you.equals("s"))) {
+		   System.out.println("自分の負けです");
+	   }
+ 
+	}	
+}
+
